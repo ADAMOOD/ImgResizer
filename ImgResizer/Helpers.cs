c705b9a20fa4ec8c1a1e74dab1237a7caddfd1dc
ADAMOOD <99761968+ADAMOOD@users.noreply.github.com>
2022-09-28 13:03:29 +0200

    Add project files.

---
+++ ImgResizer/Helpers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImgResizer
+{
+    internal class Helpers
+    {
+
+    }
+}
---
+++ ImgResizer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace ImgResizer
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string Path = null;
+            while (Path == null)
+            {
+                if (args.Length == 0)
+                {
+
+                    Path = GetPath();
+
+                }
+                else
+                {
+                    if (!Directory.Exists(args[0]))
+                    {
+                        Path = GetPath();
+                        continue;
+                    }
+                    Path = args[0];
+                }
+            }
+
+
+        }
+
+        public static string GetPath()
+        {
+            Console.WriteLine("Enter Path:");
+            string inputString = Console.ReadLine();
+            if (Directory.Exists(inputString))
+            {
+                return inputString;
+            }
+            Console.WriteLine("Invalid Path");
+            return null;
+        }
+    }
+
+}
