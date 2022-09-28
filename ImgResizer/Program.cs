bf2c21fb77d19b96178c48d091d3c156420ca8d7
ADAMOOD <99761968+ADAMOOD@users.noreply.github.com>
2022-09-28 14:29:54 +0200

    getting command

--- ImgResizer/Program.cs
+++ ImgResizer/Program.cs
@@ -1,45 +1,91 @@
 ﻿using System;
+using System.Collections.Specialized;
 using System.IO;
+using System.Linq;
 
 namespace ImgResizer
 {
     internal class Program
     {
-
+        public static string[] Commands = { "-r", "--resize", "-t", "--thumbs", "-c", "--clean" };
         static void Main(string[] args)
         {
-            string Path = null;
-            while (Path == null)
+            string path = null;
+            string command = null;
+            #region GettingInputs
+            while ((path == null) || (command == null))
             {
                 if (args.Length == 0)
                 {
-
-                    Path = GetPath();
-
+                    if (path == null)
+                    {
+                        path = GetPath();
+                    }
+                    if (command == null)
+                    {
+                        command = GetCommand();
+                    }
                 }
-                else
+                if (args.Length == 1)
                 {
                     if (!Directory.Exists(args[0]))
                     {
-                        Path = GetPath();
+                        path = GetPath();
+                        command = GetCommand();
                         continue;
                     }
-                    Path = args[0];
+                    path = args[0];
+                    command = GetCommand();
+                }
+                if (args.Length == 2)
+                {
+                    if (!Directory.Exists(args[0]))
+                    {
+                        path = GetPath();
+                        continue;
+                    }
+                    path = args[0];
+                    if (Commands.Contains(args[1]))
+                    {
+                        command = args[1];
+                        continue;
+                    }
+                    command = GetCommand();
                 }
             }
+            #endregion
 
 
         }
 
+        public static string GetCommand()
+        {
+            int x;
+            
+            Console.WriteLine("Enter command");
+            string inputCommand = Console.ReadLine();
+            if (Commands.Contains(inputCommand))
+            {
+                return inputCommand;
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid command");
+            Console.ResetColor();
+            return null;
+
+        }
         public static string GetPath()
         {
-            Console.WriteLine("Enter Path:");
+            Console.WriteLine("Enter path:");
             string inputString = Console.ReadLine();
             if (Directory.Exists(inputString))
             {
                 return inputString;
             }
-            Console.WriteLine("Invalid Path");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid path");
+            Console.ResetColor();
+            Environment.Exit(0);//Po odebrání se bude program ptát na cestu dokud uživatel nezadá existující cestu
             return null;
         }
     }
