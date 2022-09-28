221c7d515559aaac56c392a44fccdd322f3d494c
ADAMOOD <99761968+ADAMOOD@users.noreply.github.com>
2022-09-28 21:54:59 +0200

    ja uz nevim

---
+++ ImgResizer/Input.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml.Serialization;
+
+namespace ImgResizer
+{
+    public enum commands
+    {
+        resize, thumbs, clean
+    }
+    public class Input
+    {
+
+        public static string[] Commands = { "-t", "--thumbs", "-c", "--clean" };
+        public string Path = null;
+        public string Command = null;
+        public commands idk { get; set; }
+        public Input(string[] args)
+        {
+            while ((Path == null) || (Command == null))
+            {
+                if (args.Length == 0)
+                {
+                    if (Path == null)
+                    {
+                        Path = GetPath();
+                    }
+                    if (Command == null)
+                    {
+                        Command = GetCommand();
+                    }
+                }
+
+                if (args.Length > 0)
+                {
+                    if (!Directory.Exists(args[0]))
+                    {
+                        Path = GetPath();
+                        Command = GetCommand();
+                        continue;
+                    }
+                    for (int i = 1; i < args.Length; i++)
+                    {
+                        Command = CheckCommand(args[i]);
+                    }
+                }
+                /*  if (args.Length == 1)
+                  {
+                      if (!Directory.Exists(args[0]))
+                      {
+                          Path = GetPath();
+                          Command = GetCommand();
+                          continue;
+                      }
+                      Path = args[0];
+                      Command = GetCommand();
+                  }
+                  if (args.Length == 2)
+                  {
+                      if (!Directory.Exists(args[0]))
+                      {
+                          Path = GetPath();
+                          continue;
+                      }
+                      Path = args[0];
+                      if (Commands.Contains(args[1]))
+                      {
+                          Command = args[1];
+                          continue;
+                      }
+                      Command = GetCommand();
+                  }*/
+            }
+        }
+        private static string GetCommand()
+        {
+            Console.WriteLine("Enter Command");
+            string inputCommand = Console.ReadLine();
+            return CheckCommand(inputCommand);
+        }
+
+        private static string CheckCommand(string inputCommand)
+        {
+            if (inputCommand.Contains("="))
+            {
+                string[] sub = inputCommand.Split('=');
+                int num;
+                int.TryParse(sub[1], out num);
+                if (((Regex.Replace(sub[0], @"\s+", "") == "-r-w")//špinave
+                     || (Regex.Replace(sub[0], @"\s+", "") == "-resize-w")//špinave
+                     || (Regex.Replace(sub[0], @"\s+", "") == "-r--width")//špinave
+                     || (Regex.Replace(sub[0], @"\s+", "") == "-resize--width"))//špinave
+                    && ((num < 1200) && (num > 100)))
+                {
+                   
+                    return inputCommand;
+                }
+                Helpers.InvalidInput("Command");
+                Environment.Exit(0);
+                return null;
+            }
+
+            if (Commands.Contains(inputCommand))
+            {
+                return inputCommand;
+            }
+
+            Helpers.InvalidInput("Command");
+            Environment.Exit(0);
+            return null;
+        }
+
+        private static string GetPath()
+        {
+            Console.WriteLine("Enter Path:");
+            string inputString = Console.ReadLine();
+            if (Directory.Exists(inputString))
+            {
+                return inputString;
+            }
+
+            Helpers.InvalidInput("Path");
+            Environment.Exit(0);//Po odebrání se bude program ptát na cestu dokud uživatel nezadá existující cestu
+            return null;
+        }
+    }
+
+
+}
--- ImgResizer/Program.cs
+++ ImgResizer/Program.cs
@@ -3,99 +3,40 @@ using System.Collections.Specialized;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Xml.Serialization;
 
 namespace ImgResizer
 {
     internal class Program
     {
-        public static string[] Commands = { "-r", "--resize", "-t", "--thumbs", "-c", "--clean" };
+
         static void Main(string[] args)
         {
-            string path = null;
-            string command = null;
-            #region GettingInputs
-            while ((path == null) || (command == null))
+            var operation = new Input(args);
+            switch (operation.idk)
             {
-                if (args.Length == 0)
-                {
-                    if (path == null)
+                case 0:
                     {
-                        path = GetPath();
+                        if (!Directory.Exists($"{operation.Path}\\thumbs"))
+                        {
+                            Directory.CreateDirectory($"{operation.Path}\\thumbs");
+                            var allfiles = Directory.GetFiles(operation.Path).Select(x => new FileInfo(x)).ToList();
+                            foreach (var file in allfiles)
+                            {/*
+                                if ()
+                                {
+                                    
+                                }
+                                var images=*/
+                            }
+                        }
+                            break;
                     }
-                    if (command == null)
-                    {
-                        command = GetCommand();
-                    }
-                }
-                if (args.Length == 1)
-                {
-                    if (!Directory.Exists(args[0]))
-                    {
-                        path = GetPath();
-                        command = GetCommand();
-                        continue;
-                    }
-                    path = args[0];
-                    command = GetCommand();
-                }
-                if (args.Length == 2)
-                {
-                    if (!Directory.Exists(args[0]))
-                    {
-                        path = GetPath();
-                        continue;
-                    }
-                    path = args[0];
-                    if (Commands.Contains(args[1]))
-                    {
-                        command = args[1];
-                        continue;
-                    }
-                    command = GetCommand();
-                }
+
             }
-            #endregion
-
-
         }
 
-        private static string GetCommand()
-        {
-            Console.WriteLine("Enter command");
-            string inputCommand = Console.ReadLine();
-            if (inputCommand.Contains("="))
-            {
-                string[] sub = inputCommand.Split('=');
-                int num;
-                int.TryParse(sub[1], out num);
-                if (((sub[0] == "-w" || (sub[0] == "--width")) && ((num < 1200) && (num > 100))))
-                {
-                    return inputCommand;
-                }
-                Helpers.InvalidInput("command");
-                return null;
-            }
-            if (Commands.Contains(inputCommand))
-            {
-                return inputCommand;
-            }
-            Helpers.InvalidInput("command");
-            return null;
 
-        }
-        private static string GetPath()
-        {
-            Console.WriteLine("Enter path:");
-            string inputString = Console.ReadLine();
-            if (Directory.Exists(inputString))
-            {
-                return inputString;
-            }
-
-            Helpers.InvalidInput("path");
-            Environment.Exit(0);//Po odebrání se bude program ptát na cestu dokud uživatel nezadá existující cestu
-            return null;
-        }
     }
 }
